be18c3bdce890069035c3b2c7cc93139ca5c8ea8
SudipBaidyadev <139113892+SudipBaidyadev@users.noreply.github.com>
2023-11-28 11:00:00 +0530

    origin

---
+++ src/config.js
@@ -0,0 +1,7 @@
+// config.js
+const config = {
+   // apiUrl: process.env.NODE_ENV === 'production' ? 'https://womenhelpline.in/api' : '/api',
+   apiUrl: "https://womenhelpline-backend.onrender.com",
+};
+
+export default config;
